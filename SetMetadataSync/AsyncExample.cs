181aed9b8f0aaf01eaeff50b634602aa38bf75f8
Fringie <fringie@live.co.uk>
2019-07-12 21:15:49 +0100

    Async

---
+++ SetMetadataSync/AsyncExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetMetadataSync
+{
+    class AsyncExample
+    {
+        public void Main()
+        {
+            T(); // wil run instantly. synchronous
+            var t = MyMethodAsync(); // will run task, synchronous
+            L(); // wil run instantly, synchronous
+            t.Wait(); // wil wait
+            Console.WriteLine(t.Result);
+        }
+
+        void T()
+        {
+            Console.WriteLine("First");
+        }
+
+        void L()
+        {
+            Console.WriteLine("Last");
+        }
+
+        async Task<int> MyMethodAsync()
+        {
+            Task<int> longRunningTask = LongRunningOperationAsync();
+            // independent work which doesn't need the result of LongRunningOperationAsync can be done here
+
+            //and now we call await on the task 
+            int result = await longRunningTask;
+            //use the result 
+            Console.WriteLine(result);
+            return 55;
+        }
+
+        private async Task<int> LongRunningOperationAsync() // assume we return an int from this long running operation 
+        {
+            await Task.Delay(1000); // 1 second delay
+            return 1;
+        }
+    }
+}
--- SetMetadataSync/Program.cs
+++ SetMetadataSync/Program.cs
@@ -10,25 +10,11 @@ namespace SetMetadataSync
     {
         static void Main(string[] args)
         {
-            //Task<int> longRunningTask = LongRunningOperationAsync();
-
+            Engine e = new Engine();
+            e.Main();
+            Console.ReadKey();
         }
 
-        public async Task MyMethodAsync()
-        {
-            Task<int> longRunningTask = LongRunningOperationAsync();
-            // independent work which doesn't need the result of LongRunningOperationAsync can be done here
-
-            //and now we call await on the task 
-            int result = await longRunningTask;
-            //use the result 
-            Console.WriteLine(result);
-        }
-
-        public async Task<int> LongRunningOperationAsync() // assume we return an int from this long running operation 
-        {
-            await Task.Delay(1000); // 1 second delay
-            return 1;
-        }
+        
     }
 }
